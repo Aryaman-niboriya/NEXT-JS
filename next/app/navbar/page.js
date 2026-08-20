82b65779819cfabfd8584dcc9038ebffc9e47ff2
Aryaman Niboriya <aryamanniboriya94@gmail.com>
2026-08-20 23:48:15 +0530

    learn fetch api in next js

---
+++ next/app/navbar/page.js
@@ -0,0 +1,26 @@
+"use client"
+import {useState, useEffect} from "react"
+export default function Page (){
+    const [product,setproduct]=useState([]);
+    useEffect(()=>{
+        async function fetchProducts(){
+            let data=await fetch("https://dummyjson.com/products");
+            data=await data.json();
+            console.log(data);
+            setproduct(data.products)
+        }
+
+        fetchProducts()
+    },[])
+
+    return(
+        <div>
+            <h1>content api</h1>
+            {
+                product.map((item)=>{
+                    return <h2 key={item.id}>{item.title}</h2>
+                })
+            }
+        </div>
+    )
+}
